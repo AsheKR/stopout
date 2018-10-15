9de78b1cf1d22e31fcecaaf8b49f19d61216b1c4
teachmesomething2580 <teachmesomething2580@gmail.com>
2018-10-14 19:16:15 -0700

    school model 작성

--- app/school/models.py
+++ app/school/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+
+class School(models.Model):
+    school_name = models.CharField(max_length=80)
+
+
+class Student(models.Model):
+    student_name = models.CharField(max_length=80)
+    now_school = models.ForeignKey(
+        School,
+        on_delete=models.CASCADE,
+    )
+    best_friend = models.ManyToManyField(
+        'self',
+        on_delete=models.CASCADE,
+    )
